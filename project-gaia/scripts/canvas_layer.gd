6197e30ba54d4e2e90f240ff64066b115e821a4e
Mastergamerrrr <labortejian5@gmail.com>
2025-11-02 02:32:22 +0800

    Add new audio, sprite assets and update canvas layer
    Added teleport sound, background music, and water effect/bullet sprite assets with import files. Updated canvas_layer scene to use new assets, added animated sprites and audio stream players, and extended the script to handle animation and sound playback logic.

--- project-gaia/scripts/canvas_layer.gd
+++ project-gaia/scripts/canvas_layer.gd
@@ -1,4 +1,7 @@
 extends CanvasLayer
+@onready var aud: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var anim: AnimatedSprite2D = $AnimatableBody2D
+@onready var anima: AnimatedSprite2D = $AnimatableBody2D2
 
 func _ready():
 	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
@@ -9,3 +12,9 @@ func fade_to_black():
 func _on_animation_finished(anim_name):
 	if anim_name == "fadeout":
 		get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func play():
+	anima.visible = false
+	anim.visible = true
+	aud.play()
+	anim.play()
